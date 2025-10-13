00bff9d732f78caaf38652156232aec020cc86ea
Sandeep Sharma <sandeeptech8@gmail.com>
2025-10-13 17:08:03 +0530

    feat: register django cors headers package in project settings

--- snippets/settings.py
+++ snippets/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'core',
     'drf_spectacular',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'snippets.urls'
