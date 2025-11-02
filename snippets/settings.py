0ef5aa2fe73279133dee481c23cd1d4e6059a9a7
Sandeep Sharma <sandeeptech8@gmail.com>
2025-11-02 16:39:36 +0530

    chore: add localhost to allowed hosts list

--- snippets/settings.py
+++ snippets/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$++pi5%1upmiyj(%+j2-j@#z#c0*a0n=bkuuug@wjmbmdpl4h-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
