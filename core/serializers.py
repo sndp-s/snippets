61eb89a5be52342304c883e9dae80a877b2cb19f
Sandeep Sharma <sandeeptech8@gmail.com>
2025-11-05 19:00:53 +0530

    feat: return tags along with each snippet in get all snippets endpoint

--- core/serializers.py
+++ core/serializers.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 from .models import Snippet, Tag
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Snippet
-        fields = ['id', 'title', 'text', 'parent', 'created_dt', 'updated_dt']
-
-
 class TagSerializer(serializers.ModelSerializer):
     snippet_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Tag
         fields = ['id', 'name', 'snippet_count', 'created_dt', 'updated_dt']
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'text', 'parent',
+                  'tags', 'created_dt', 'updated_dt']
